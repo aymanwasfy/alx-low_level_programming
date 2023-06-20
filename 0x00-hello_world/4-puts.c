037dce3cbb8cc55a41102be08525c8cee90acb3f
aymanwasfy <ayman.waasfy@gmail.com>
2023-06-19 18:53:36 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio>
+
+/**
+ * main - Entry point
+ *
+ * Decription:C program that prints with puts function
+ *
+ * Return: Alawys 0 (success)
+ */
+int main(void)
+{
+puts("\""Programming is like building a multilingual puzzle\n");
+return(0);
+}
+

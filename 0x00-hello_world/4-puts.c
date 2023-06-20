2f21f17ede783d72ee17346c223b872d546e99ae
aymanwasfy <ayman.waasfy@gmail.com>
2023-06-19 19:08:19 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,5 @@
-#include <stdio>
+#include <stdio.h>
+
 
 /**
  * main - Entry point
@@ -7,9 +8,12 @@
  *
  * Return: Alawys 0 (success)
  */
+
 int main(void)
 {
-puts("\""Programming is like building a multilingual puzzle\n");
-return(0);
+puts("\"Programming is like building a multilingual puzzle\n ");
+	return(0);
+
+
 }
 

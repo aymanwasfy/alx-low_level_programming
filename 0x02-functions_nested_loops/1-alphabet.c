ae983dc5149509eff3480a8525b000b340b0dc64
aymanwasfy <ayman.waasfy@gmail.com>
2023-06-21 18:41:55 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet - Entry point
+ * Description: prints the alphabet, in lowercase, followed by a new line.
+ */
+
+void print_alphabet(void)
+{
+int ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+	_putchar(ch);
+_putchar('\n');
+}
